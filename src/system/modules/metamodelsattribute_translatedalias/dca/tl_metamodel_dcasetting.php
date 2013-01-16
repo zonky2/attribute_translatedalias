f513e0f9b2de2e59e48b9053da9e33d3bb7c9709
Stefan Heimes <stefan_heimes@hotmail.com>
2013-01-16 19:29:19 +0100

    Update for the dcasettings (Paletteneinstellunegn), change the order of the options, create some new groups (presentations, function, overview), add some missing dca_settings

---
+++ src/system/modules/metamodelsattribute_translatedalias/dca/tl_metamodel_dcasetting.php
@@ -0,0 +1,20 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['translatedalias'] =
+array
+(    
+    'presentation' => array(
+        'tl_class',       
+    ),
+    'functions'  => array(
+        'mandatory',
+    ),
+	'overview' => array(		
+		'filterable',
+		'searchable',
+		'sortable',
+		'flag'
+	)
+);
+
+?>
