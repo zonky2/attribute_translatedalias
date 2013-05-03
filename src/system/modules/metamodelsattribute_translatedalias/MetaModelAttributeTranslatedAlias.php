86d2230d5cffc456ee5645f6731efcb7fb370efa
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-05-04 00:53:39 +0200

    Fixed issue #3 - the alias was not being generated language dependant but over all languages.

--- src/system/modules/metamodelsattribute_translatedalias/MetaModelAttributeTranslatedAlias.php
+++ src/system/modules/metamodelsattribute_translatedalias/MetaModelAttributeTranslatedAlias.php
@@ -70,12 +70,13 @@ class MetaModelAttributeTranslatedAlias extends MetaModelAttributeTranslatedRefe
 		// implode with '-'
 		$strAlias  = standardize(implode('-', $arrAlias));
 
+		$strLanguage = $this->getMetaModel()->getActiveLanguage();
 		// we need to fetch the attribute values for all attribs in the alias_fields and update the database and the model accordingly.
-		if ($this->get('isunique') && $this->searchFor($strAlias))
+		if ($this->get('isunique') && $this->searchForInLanguages($strAlias, $strLanguage))
 		{
 			$intCount = 1;
 			// ensure uniqueness.
-			while (count($this->searchFor($strAlias . '-' . (++$intCount))) > 0){}
+			while (count($this->searchForInLanguages($strAlias . '-' . (++$intCount), $strLanguage)) > 0){}
 			$strAlias = $strAlias . '-' . $intCount;
 		}
 
