0127b79d02c2746afb0178b012f2a6d77f9d5cb5
Andreas Isaak <andy.jared@googlemail.com>
2014-01-20 20:45:53 +0100

    Delete unused comma

--- src/system/modules/metamodelsattribute_translatedalias/config/database.sql
+++ src/system/modules/metamodelsattribute_translatedalias/config/database.sql
@@ -19,9 +19,9 @@ CREATE TABLE `tl_metamodel_translatedtext` (
   `langcode` varchar(2) NOT NULL default '',
   `value` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`),
-  KEY `attvalue` (`att_id`, `value`)
+  KEY `attvalue` (`att_id`, `value`),
   KEY `attlang` (`att_id`, `langcode`),
-  KEY `attitem` (`att_id`, `item_id`),
+  KEY `attitem` (`att_id`, `item_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -30,5 +30,5 @@ CREATE TABLE `tl_metamodel_translatedtext` (
 
 CREATE TABLE `tl_metamodel_attribute` (
   `talias_fields` blob NULL,
-  `force_talias` char(1) NOT NULL default '',
+  `force_talias` char(1) NOT NULL default ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
